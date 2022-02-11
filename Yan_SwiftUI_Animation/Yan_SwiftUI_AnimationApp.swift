3208271a63cb6b0b9c15f3032efec977a07b1120
Kir Proto Q <kir.proto@ya.ru>
2022-02-11 06:43:05 +0500

    Initial Commit

---
+++ Yan_SwiftUI_Animation/Yan_SwiftUI_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yan_SwiftUI_AnimationApp.swift
+//  Yan_SwiftUI_Animation
+//
+//  Created by KIR Q on 2022.02.11.
+//
+
+import SwiftUI
+
+@main
+struct Yan_SwiftUI_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
